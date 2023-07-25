6516d4ae1597b57d184d311f16846b085b06a409
vijaytkd-VA <127473876+vijaytkd-VA@users.noreply.github.com>
2023-07-25 19:23:04 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
+}
+
